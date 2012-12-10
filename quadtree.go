e7127116bd4febe0411f582250eebfa4e46bfc07
valens_a <arnaud.valensi@gmail.com>
2012-12-10 02:31:44 +0100

    QuadTree type finished but not tested

--- quadtree.go
+++ quadtree.go
@@ -25,19 +25,49 @@ func (this *QuadTree) GetWorldRect() Rect {
 }
 
 func (this *QuadTree) Insert(item *PositionItem) {
-
+        // check if the world needs resizing
+        if (!this.rootNode.ContainsRect(item.GetRect())) {
+		min := rootNode.Rect.TopLeft.Min(item.Rect.TopLeft)
+		max := rootNode.Rect.BottomRight.Max(item.Rect.BottomRight)
+		min.Mult(2)
+		max.Mult(2)
+		rect := NewRect(min, max)
+                this.Resize(rect)
+        }
+        rootNode.Insert(item);
 }
 
-func (this *QuadTree) Remove() {
+func (this *QuadTree) Resize(newWorld *Rect) {
+        // Get all of the items in the tree
+        // List<QuadTreePositionItem<T>> Components = new List<QuadTreePositionItem<T>>();
+	itemList := List.Init()
+	this.GetAllItems(itemList);
 
+        // Create a new head
+        this.rootNode = NewQuadTreeNode(nil, newWorld, this.maxItems)
+
+	for e := itemList.Front(); e != nil; e = e.Next() {
+		this.rootNode.Insert(e.(PositionItem))
+	}
 }
 
 // Gets a list of items containing a specified point
 func (this *QuadTree) GetItemsFromPoint(point *Position, itemsList *List) {
-
+        if itemsList != null {
+                this.rootNode.GetItemsFromPoint(point, itemsList);
+        }
 }
 
 // Gets a list of items intersecting a specified rectangle
 func (this *QuadTree) GetItemsFromRect(rect *Rect, itemsList *List) {
-
+        if itemsList != null {
+                this.rootNode.GetItemsFromRect(point, itemsList);
+        }
 }
+
+func (this *QuadTree) GetAllItems(itemList *List.list) {
+        if itemsList != null) {
+                this.rootNode.GetAllItems(itemList);
+        }
+}
+
--- rect.go
+++ rect.go
@@ -2,12 +2,55 @@ package goquadtree
 
 type Rect struct {
 	TopLeft		Position
-	TopRight	Position
-	BottomLeft	Position
+	// TopRight	Position
+	// BottomLeft	Position
 	BottomRight	Position
+}
+
+func NewRect(topLeft *Position, bottomRight *Position) *Rect {
+	return &Rect {
+		topLeft,
+		bottomRight
+	}
 }
 
 type Position struct {
 	X		int
 	Y		int
+}
+
+func NewPosition(x int, y int) *Position {
+	return &Position {
+		x,
+		y
+	}
+}
+
+func (this *Position) Min(pos *Position) {
+	if this.X > pos.X {
+		x := pos.X
+	} else {
+		x := this.X
+	}
+	
+	if this.Y > pos.Y {
+		y := pos.Y
+	} else {
+		y := this.Y
+	}
+
+	return &Position {
+		x,
+		y
+	}
+}
+
+func (this *Position) Mult(n int) {
+	this.X = this.X * n
+	this.Y = this.Y * n
+}
+
+func (this *Position) Div(n int) {
+	this.X = this.X / n
+	this.Y = this.Y / n
 }
